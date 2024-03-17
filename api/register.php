70d3b5e0dab12b66006049572bf74e4b4db6ed13
Fernando Ramos <feramos60@gmail.com>
2024-03-16 22:31:26 -0500

    Se cuadra en la api la recepcion del registro

--- api/register.php
+++ api/register.php
@@ -13,7 +13,7 @@ if ($_SERVER["REQUEST_METHOD"] !== "POST") {
 
 $data = (array) json_decode(file_get_contents("php://input"), true);
 
-$requiredFields = ['user_name', 'password', 'first_name', 'last_name', 'email', 'phone'];
+$requiredFields = ['username', 'firstname', 'lastname', 'email', 'phone', 'password'];
 foreach ($requiredFields as $field) {
     if (! array_key_exists($field, $data)) {
         http_response_code(400);
@@ -31,16 +31,22 @@ $user_gateway = new UserGateway($database);
 
 $user_id= $user_gateway->create($data, $_ENV["SECRET_KEY_PASS"]);
 
-if ($user_id > 0) {
+$id = intval($user_id['id']);
 
-    $send = $user_gateway->sendActivationEmail($data);
+if ($id > 0) {
+
+    $send = $user_gateway->sendActivationEmail($data, $user_id['api_key']);
     
     if ($send) {
-        echo json_encode(['message' => 'user created and activation email sent ID: '. $user_id]);
+        echo json_encode(['message' => 'user created and activation email sent ID: '. $id]);
         exit;
     }   
 
     http_response_code(400);
     echo json_encode(['message' => 'incomplete registration no se envio nada '. $send]);
+    exit;
+} else {
+    http_response_code(400);
+    echo json_encode(['message' => 'user exist']);
     exit;
 }
--- src/UserGateway.php
+++ src/UserGateway.php
@@ -1,14 +1,19 @@
 <?php
 
+
+
 class UserGateway
 {
     private PDO $conn;
-    private $token;
+    public $token;
     private $api_key;
+    public $activation_token;
+    private $hashed_token;
     
     public function __construct(Database $database)
     {
         $this->conn = $database->getConnection();
+       
     }
     
     public function getByAPIKey(string $key): array | false
@@ -56,41 +61,53 @@ class UserGateway
         return $stmt->fetch(PDO::FETCH_ASSOC);
     }
 
-    public function create(array $data, string $secret): string
+    public function create(array $data, string $secret): array | int
     {
+        $array = [];
         $password_hash = password_hash($data["password"], PASSWORD_DEFAULT);
-        $this->token = bin2hex(random_bytes(16)); 
-        $para_token = strval($this->token);
-        $this->api_key = hash_hmac('sha256', $para_token, $secret);
+        $token = bin2hex(random_bytes(16));
+        $hashed_token = strval($token);
+        $hashed_token = hash_hmac('sha256', $hashed_token, $secret);
+        $this->activation_token = $token;
 
         $sql = 'INSERT INTO users (user_name, first_name, last_name, email, phone, role_id, api_key, password_hash, activation_hash)
                     VALUES (:user_name, :first_name, :last_name, :email, :phone, :role_id, :api_key, :password_hash, :activation_hash)';
                 
         $stmt = $this->conn->prepare($sql);
-        $stmt->bindValue(":user_name", $data["user_name"], PDO::PARAM_STR);
-        $stmt->bindValue(":first_name", $data["first_name"], PDO::PARAM_STR);
-        $stmt->bindValue(":last_name", $data["last_name"], PDO::PARAM_STR);
+        $stmt->bindValue(":user_name", $data["username"], PDO::PARAM_STR);
+        $stmt->bindValue(":first_name", $data["firstname"], PDO::PARAM_STR);
+        $stmt->bindValue(":last_name", $data["lastname"], PDO::PARAM_STR);
         $stmt->bindValue(":email", $data["email"], PDO::PARAM_STR);
         $stmt->bindValue(":phone", $data["phone"], PDO::PARAM_STR);
         $stmt->bindValue(":role_id", 3, PDO::PARAM_INT);
-        $stmt->bindValue(":api_key", $this->api_key, PDO::PARAM_STR);
+        $stmt->bindValue(":api_key", $hashed_token, PDO::PARAM_STR);
         $stmt->bindValue(":password_hash", $password_hash, PDO::PARAM_STR);
-        $stmt->bindValue(':activation_hash', $this->api_key, PDO::PARAM_STR);
-        $stmt->execute();        
-        return $this->conn->lastInsertId();
+        $stmt->bindValue(':activation_hash', $hashed_token, PDO::PARAM_STR);
+        try {
+            $stmt->execute();
+            $array = [
+                "id" => $this->conn->lastInsertId(),
+                "api_key" => $this->activation_token
+            ];
+            return $array;
+        } catch (\Throwable $th) {
+            return 0;
+        }
+                
+        
     }
 
-    function sendActivationEmail(array $data) : bool
+    function sendActivationEmail(array $data, string $secret) : bool
     {
-        $url = 'https://' . $_SERVER['HTTP_HOST'] . '/legaltech/compa/signup/activate/' . $this->api_key;
+        $url = 'https://' . $_SERVER['HTTP_HOST'] . '/legaltech/compa/signup/activate/' . $secret;
         // Leer el contenido del archivo activation_email.html
         $html = file_get_contents('./activation_email.html');
 
         // Reemplazar las variables en el HTML con los valores correspondientes
         $html = str_replace('{{url}}', $url, $html);
-        $html = str_replace('{{nombre}}', $data['first_name'], $html);
+        $html = str_replace('{{nombre}}', $data['firstname'], $html);
        
-        $send = Mail::send($data["email"], $data['first_name'], 'Activación de la cuenta - Ecoapplet', $html);
+        $send = Mail::send($data["email"], $data['firstname'], 'Activación de la cuenta - Ecoapplet', $html);
         return $send;
     }
 }
